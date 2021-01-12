86b12a615dcd724eee32a9f7b97fcd4a7e1f7757
vy <vyht444@gmail.com>
2021-01-12 16:24:23 -0600

    update log to file

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -5,30 +5,36 @@ from utilities.BaseClass import BaseClass
 class TestShopping(BaseClass):
 
     def test_e2e(self):
-        #log
+        log = self.getLogger()
+
         homePage = HomePage(self.driver)
         shopPage = homePage.shopItems()
-        #log
+        log.info("Get all card titles")
         cards = shopPage.getCardTitles()
 
         i = -1
         for card in cards:
             i = i + 1
             cardText = card.text
-            #log
+            log.info(cardText)
             if cardText == "Nokia Edge":
                 shopPage.getCardFooters()[i].click()
 
         shopPage.CheckOutItems().click()
         confirmPage = shopPage.confirmCheckOut()
-        #log
+
+        log.info("Search country name with un")
         confirmPage.searchCountry(text="un")
+
         self.verifyLinkPresence(linkText="United States of America")
+
         self.driver.find_element_by_link_text("United States of America").click()
         confirmPage.getAgreeCheckBox().click()
         confirmPage.submitPurchase().click()
+
         successAlertText = confirmPage.getSuccessAlert()
-        #log
+        log.info("Text from the confirmation page" + successAlertText)
+
         assert ("Success! Thank you" in successAlertText)
 
 
--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -1,4 +1,7 @@
+import inspect
+
 import pytest
+import logging
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,6 +9,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
+
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+        fileHandler.setFormatter(formatter)
+
+        logger.addHandler(fileHandler)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+
     def verifyLinkPresence(self, linkText):
         wait = WebDriverWait(self.driver, 10)
         wait.until(EC.presence_of_element_located((By.LINK_TEXT, linkText)))
